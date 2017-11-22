b495208236dcfaa44f6c3ab8b3ff8e546663f21f
Max O'Donnel <screwyprof@gmail.com>
2017-11-22 20:28:26 +0300

    Specifications added

---
+++ entity/spec/only_page.go
@@ -0,0 +1,20 @@
+package spec
+
+//type OnlyPageSpec struct {
+//	page int
+//	itemsPerPage int
+//
+//}
+//
+//func NewOnlyPageSpec(page, itemsPerPage int) OnlyPageSpec {
+//	return OnlyPageSpec{page:page, itemsPerPage:itemsPerPage}
+//}
+
+//func (s OnlyPageSpec) IsSatisfiedBy() {
+//
+//}
+
+//public function match(Query $query)
+//{
+//return $query->limit($this->itemsPerPage)->offset($this->page);
+//}
