73e2f73e4194d6a186b36f49809a664fe13d9e5c
Max O'Donnel <screwyprof@gmail.com>
2017-11-23 18:19:39 +0300

    Query DTO tests added

--- usecase/marketplace/query/query_test.go
+++ usecase/marketplace/query/query_test.go
@@ -1,7 +1,20 @@
 package query
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
 type unknownQuery struct{}
 
 func (c unknownQuery) QueryID() string {
 	return "UnknownQuery"
+}
+
+func TestGetOrder_QueryID(t *testing.T) {
+	assert.Equal(t, "GetOrder", GetOrder{}.QueryID())
+}
+
+func TestGetOrders_QueryID(t *testing.T) {
+	assert.Equal(t, "GetOrders", GetOrders{}.QueryID())
 }
