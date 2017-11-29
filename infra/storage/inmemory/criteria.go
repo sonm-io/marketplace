b255e54f137b6d9ed01d75b0dcca0b62f8ce2b47
Max O'Donnel <screwyprof@gmail.com>
2017-11-29 19:54:48 +0300

    Infra lint warnings fixed

--- infra/storage/inmemory/criteria.go
+++ infra/storage/inmemory/criteria.go
@@ -1,9 +1,11 @@
 package inmemory
 
+// Specification represents a Specification pattern.
 type Specification interface {
 	IsSatisfiedBy(object interface{}) bool
 }
 
+// ConcreteCriteria is used a parameter object to encapsulate Criteria to match against.
 type ConcreteCriteria struct {
 	Limit uint64
 	Spec  Specification
--- infra/storage/inmemory/criteria_builder.go
+++ infra/storage/inmemory/criteria_builder.go
@@ -1,18 +1,22 @@
 package inmemory
 
+// CriteriaBuilder helps building arbitrary Criteria objects.
 type CriteriaBuilder struct {
 	limit uint64
 	spec  Specification
 }
 
+// NewBuilder creates a new instance of CriteriaBuilder.
 func NewBuilder() *CriteriaBuilder {
 	return &CriteriaBuilder{}
 }
 
+// WithLimit adds a limit to the Criteria.
 func (b *CriteriaBuilder) WithLimit(limit uint64) {
 	b.limit = limit
 }
 
+// WithSpec adds a Specification to the Criteria.
 func (b *CriteriaBuilder) WithSpec(spec Specification) {
 	if spec == nil {
 		return
@@ -20,6 +24,7 @@ func (b *CriteriaBuilder) WithSpec(spec Specification) {
 	b.spec = spec
 }
 
+// Build builds the Criteria object.
 func (b *CriteriaBuilder) Build() ConcreteCriteria {
 	c := ConcreteCriteria{}
 	c.Spec = b.spec
